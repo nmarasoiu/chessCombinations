44815192bd754ca131a8cf19922dbf5c87220c37
nicu <nicolae.marasoiu@gmail.com>
2019-03-01 19:15:48 +0200

    fully developed tests for the two examples in homework; to add properties based tests & limit case tests with small/zero/negative tables and positions etc

--- src/main/scala/chess/GenerationCore.scala
+++ src/main/scala/chess/GenerationCore.scala
@@ -1,25 +1,16 @@
 package chess
 
-import java.util.Optional
-
 object GenerationCore {
   def solutions(input: Input): Stream[Solution] = {
-    input match {
-      case Input(table: Table, piecesCount: Map[ChessPiece, Int], positions: Set[Position]) => {
-        val s: Stream[Stream[Solution]] =
-          for (piece: ChessPiece <- piecesCount.keySet.toStream if piecesCount(piece) > 0;
-               position: Position <- positions;
-               remainingPiecesCount: Map[ChessPiece, Int] = piecesCount.updated(piece, piecesCount(piece) - 1);
-               remainingPositions: Set[Position] = positions - position -- piece.attackPositions(position, input.table);
-               smallerInput: Input = Input(table, remainingPiecesCount, remainingPositions))
-            yield {
-              val s1: Stream[Solution] = solutions(smallerInput)
-              s1
-            }
-        val fl: Stream[Solution] = s.flatten
-        fl
-      }
-    }
+    val Input(table, piecesCount, positions) = input
+    (for (piece <- piecesCount.keySet.toStream if piecesCount(piece) > 0;
+          position <- positions;
+          remainingPiecesCount = piecesCount.updated(piece, piecesCount(piece) - 1);
+          remainingPositions = positions - position -- piece.attackPositions(position, input.table);
+          smallerInput = Input(table, remainingPiecesCount, remainingPositions))
+      yield {
+        solutions(smallerInput)
+      }).flatten
   }
 }
 
--- src/test/scala/ChessProperties.scala
+++ src/test/scala/ChessProperties.scala
@@ -1,33 +1,59 @@
 import chess.ChessPiece._
-import chess.{GenerationCore, Input, Table}
-import org.scalacheck.{Arbitrary, Properties}
+import chess._
 import org.scalacheck.Prop.forAll
 import org.scalacheck.ScalacheckShapeless._
+import org.scalacheck.{Arbitrary, Properties}
 
 object ChessProperties extends Properties("GenerationCore") {
   implicitly[Arbitrary[Input]]
-/*
-  property("solutions") = forAll { input: Input => {
-    val size = GenerationCore.solutions(input).size
-    assert(size >= 0)
-    println("Generated " + size + " solutions")
-    size >= 0
+
+  def rotations(solution: List[(ChessPiece, (Int, Int))]): List[List[(ChessPiece, (Int, Int))]] =
+    Stream.iterate(solution)(rotation).take(4).toList
+
+  def rotation(solution: List[(ChessPiece, (Int, Int))]): List[(ChessPiece, (Int, Int))] = {
+    def rotation(x: Int, y: Int): (Int, Int) = (x, y) //todo implement
+    for ((piece, (x, y)) <- solution) yield (piece, rotation(x, y))
   }
-  }
-*/
 
   property("example1") = forAll { _: Unit => {
     println("Example1:")
-    val solutions = GenerationCore.solutions(Input(Table(3, 3), Map(King -> 2, Rook -> 1)))
-    println("Example1: solutions: \n"+solutions.mkString("\n"))
-    solutions.size == 4
+    val input = Input(Table(3, 3), Map(King -> 2, Rook -> 1))
+    val expectedBoards: List[List[(ChessPiece, (Int, Int))]] = List(List((Rook, (1, 0)), (King, (0, 2)), (King, (2, 2))))
+
+    areResultingBoardsTheExpectedOnes(input, expectedBoards)
   }
   }
+
   property("example2") = forAll { _: Unit => {
     println("Example2:")
-    val solutions = GenerationCore.solutions(Input(Table(4, 4), Map(Rook -> 2, Knight -> 4)))
-    println("Example2: solutions: \n"+solutions.mkString("\n"))
-    solutions.size == 8
+    val input = Input(Table(4, 4), Map(Rook -> 2, Knight -> 4))
+    val expectedBoards: List[List[(ChessPiece, (Int, Int))]] =
+      List(
+        List((Rook, (0, 0)), (Knight, (1, 1)), (Knight, (3, 1)), (Rook, (2, 2)), (Knight, (1, 3)), (Knight, (3, 3))),
+        List((Rook, (2, 0)), (Knight, (1, 1)), (Knight, (3, 1)), (Rook, (0, 2)), (Knight, (1, 3)), (Knight, (3, 3))))
+
+    areResultingBoardsTheExpectedOnes(input, expectedBoards)
   }
   }
+
+  private def areResultingBoardsTheExpectedOnes
+  (input: Input, expectedBoards: List[List[(ChessPiece, (Int, Int))]]): Boolean = {
+    val solutions = GenerationCore.solutions(input)
+    val obtainedSolutions = solutions.map(_.solution.toList.map {
+      case (piece, Position(x, y)) => (piece, (x, y))
+    })
+    println("Example1: solutions: \n" + obtainedSolutions.mkString("\n"))
+    obtainedSolutions.toSet == expectedBoards.flatMap(rotations).toSet
+  }
+
+
+  /*
+    property("solutions") = forAll { input: Input => {
+      val size = GenerationCore.solutions(input).size
+      assert(size >= 0)
+      println("Generated " + size + " solutions")
+      size >= 0
+    }
+    }
+  */
 }
