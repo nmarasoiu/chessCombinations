bcfc81561ec0b01eab772874f9df5dfcb35711d6
nicu <nicolae.marasoiu@gmail.com>
2019-03-06 18:57:53 +0200

    simplified; solutions are now emitted in leaf calls, from picksSoFar

--- src/main/scala/chess/GenerationCore.scala
+++ src/main/scala/chess/GenerationCore.scala
@@ -18,39 +18,30 @@ object GenerationCore {
 
   private implicit val ec = ExecutionContext.global
 
-  private def _solutions(input: Input)(picksSoFar: Set[Position])(minPositionByPiece: Map[Piece, Position]): Future[Seq[PotentialSolution]] = {
+  private def _solutions(input: Input)(picksSoFar: Set[PiecePosition])(minPositionByPiece: Map[Piece, Position]): Future[Seq[PotentialSolution]] = {
     val Input(table, pieces: Seq[Piece], positions: Positions) = input
 
     def __solutions(piece: Piece, minPositionForPiece: Position, remainingPieces: Seq[Piece]): Future[Seq[PotentialSolution]] = {
-      val r: Seq[Future[Seq[PotentialSolution]]] =
-        for (position: PositionInt <- positions.filter(pos => pos >= minPositionForPiece).toSeq; //todo try BitSet.range intersect with positions?
+      val seq: Seq[Future[Seq[PotentialSolution]]] =
+        for (position <- positions.toSeq if position >= minPositionForPiece;
+             _ <- Seq(1) if !picksSoFar.exists { case PiecePosition(_, otherPosition) => piece.takes(position, otherPosition) };
              incompatiblePositions = piece.attackPositions(position, table);
-             _ <- Seq(1) if !picksSoFar.exists(otherPosition => piece.takes(position, otherPosition));
-             remainingPositions = positions - position &~ incompatiblePositions)
-          yield {
-            val piecePosition = PiecePosition(piece, position)
-            if (remainingPieces.isEmpty) {
-              Future.successful(Seq(PotentialSolution(Set(piecePosition))))
-            } else {
-              val remainingInput = Input(table, remainingPieces, remainingPositions)
-              val remainingPotentialSolutions: Future[Seq[PotentialSolution]] =
-                _solutions(remainingInput)(picksSoFar + position)(minPositionByPiece.updated(piece, position + 1))
-
-              remainingPotentialSolutions.map(_.map(remainingPotentialSolution => {
-                PotentialSolution(remainingPotentialSolution.solution + piecePosition)
-              }))
-            }
-          }
-      Future.sequence(r).map(_.flatten)
+             remainingPositions = positions - position &~ incompatiblePositions;
+             remainingInput = Input(table, remainingPieces, remainingPositions);
+             remainingMinPosByPiece = minPositionByPiece.updated(piece, position + 1);
+             newPicks = picksSoFar + PiecePosition(piece, position))
+          yield _solutions(remainingInput)(newPicks)(remainingMinPosByPiece)
+      Future.sequence(seq).map(_.flatten)
     }
 
     if (pieces.isEmpty || table.vertical <= 0 || table.horizontal <= 0) {
-      Future.successful(Seq())
+      Future.successful(Seq(PotentialSolution(picksSoFar)))
     } else {
       val piece: Piece = pieces.head
       val minPositionForPiece = minPositionByPiece(piece)
       val remainingPieces: Seq[Piece] = pieces.tail
-      val eventualSolutionsSupplier: () => Future[Seq[PotentialSolution]] = () => __solutions(piece, minPositionForPiece, remainingPieces)
+      val eventualSolutionsSupplier: () => Future[Seq[PotentialSolution]] =
+        () => __solutions(piece, minPositionForPiece, remainingPieces)
       if (remainingPieces.size > 2 && remainingPieces.size * positions.size > 40) {
         Future(eventualSolutionsSupplier.apply()).flatten
       } else {
