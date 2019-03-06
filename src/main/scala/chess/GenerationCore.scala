b8950871d68565f544c988716b445e1e42fe7c31
nicu <nicolae.marasoiu@gmail.com>
2019-03-06 13:33:56 +0200

    start of attempt to parralelisation; will add threashold to only parralelise bigger tasks

--- src/main/scala/chess/GenerationCore.scala
+++ src/main/scala/chess/GenerationCore.scala
@@ -1,5 +1,8 @@
 package chess
 
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+
 /**
   * I still have two major things to do, which I am doing now, and I will send them as soon as I can:
   * - remove permutations & rotations! impose an order on the piecePostion list of the solution, to only search in that order; was: to figure out a way to not recompute solutions multiple times (which will improve orders of magnitude)
@@ -18,36 +21,40 @@ object GenerationCore {
     */
   //todo in parallel? thread safe? .par but..
   def solutions(input: Input): Seq[PotentialSolution] = {
-    _solutions(input)(Set())(Map().withDefaultValue(0)).filter(sol => sol.solution.size == input.pieces.size)
+    Await.result(_solutions(input)(Set())(Map().withDefaultValue(0)), Duration.Inf)
+      .filter(sol => sol.solution.size == input.pieces.size)
+
   }
 
-  private def _solutions(input: Input)(picksSoFar: Set[Position])(minPositionByPiece: Map[Piece, Position]): Seq[PotentialSolution] = {
+  private def _solutions(input: Input)(picksSoFar: Set[Position])(minPositionByPiece: Map[Piece, Position]): Future[Seq[PotentialSolution]] = {
     val Input(table, pieces: Seq[Piece], positions: Positions) = input
     if (pieces.isEmpty || table.vertical <= 0 || table.horizontal <= 0) {
-      Seq()
+      Future.successful(Seq())
     } else {
+      implicit val ec = ExecutionContext.global
       val piece: Piece = pieces.head
       val minPositionForPiece = minPositionByPiece(piece)
       val remainingPieces: Seq[Piece] = pieces.tail
-      val r: Seq[Iterable[PotentialSolution]] =
-        for (position: PositionInt <- positions.filter(pos => pos >= minPositionForPiece).toSeq;//todo try BitSet.range intersect with positions?
+      val r: Seq[Future[Seq[PotentialSolution]]] =
+        for (position: PositionInt <- positions.filter(pos => pos >= minPositionForPiece).toSeq; //todo try BitSet.range intersect with positions?
              incompatiblePositions = piece.attackPositions(position, table);
              _ <- Seq(1) if !picksSoFar.exists(otherPosition => piece.takes(position, otherPosition));
              remainingPositions = positions - position &~ incompatiblePositions)
           yield {
             val piecePosition = PiecePosition(piece, position)
             if (remainingPieces.isEmpty) {
-              Set(PotentialSolution(Set(piecePosition)))
+              Future.successful(Seq(PotentialSolution(Set(piecePosition))))
             } else {
               val remainingInput = Input(table, remainingPieces, remainingPositions)
-              val remainingPotentialSolutions = _solutions(remainingInput)(picksSoFar + position)(minPositionByPiece.updated(piece, position + 1))
+              val remainingPotentialSolutions: Future[Seq[PotentialSolution]] =
+                _solutions(remainingInput)(picksSoFar + position)(minPositionByPiece.updated(piece, position + 1))
 
-              remainingPotentialSolutions.map(remainingPotentialSolution => {
+              remainingPotentialSolutions.map(_.map(remainingPotentialSolution => {
                 PotentialSolution(remainingPotentialSolution.solution + piecePosition)
-              })
+              }))
             }
           }
-      r.flatten
+      Future.sequence(r).map(_.flatten)
     }
   }
 }
