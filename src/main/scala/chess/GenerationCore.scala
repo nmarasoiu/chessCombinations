4280363f0a73938c2e42493d3bec1b87bce27f95
nicu <nicolae.marasoiu@gmail.com>
2019-03-06 11:44:08 +0200

    updated todos

--- src/main/scala/chess/GenerationCore.scala
+++ src/main/scala/chess/GenerationCore.scala
@@ -3,16 +3,12 @@ package chess
 object GenerationCore {
   /**
     * todo:
-    * replace Set[Position] with BitSet everywhere to make the set construction faster & set-set O(1), i.e. refactoring Piece logic to PieceSet a type alias of BitSet (immutable?)
-    * remove/replace more streams: e.g. replace Stream[Piece] with Map[Piece,Int]
+    * proceed in the order of pieces that eliminate more positions, to minimize recursive search space
     * in parallel: course grained, split the table, do .par on some collections, careful on granularity
     * scala test, add tests
-    * anything to cache/reuse, dynamic prog?
     * refactor into a single for and get rid of flatten?
     * inspect & remove todos
     * refactoring, beautiful well organized code
-    * memory pressure, balance lazy with eager
-    * too many sets? preallocate?
     */
   //todo in parallel? thread safe? .par but..
   def solutions(input: Input): Seq[PotentialSolution] = {
