3a6599b5378cd643de8987100885d37b3d9a2b34
nicu <nicolae.marasoiu@gmail.com>
2019-03-06 14:42:31 +0200

    limiting the parallelism

--- src/main/scala/chess/GenerationCore.scala
+++ src/main/scala/chess/GenerationCore.scala
@@ -1,12 +1,16 @@
 package chess
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.Try
 
 object GenerationCore {
   /**
     * todo:
     * - remove OOM
+    * - error management on the async
     * in parallel: course grained, split the table, do .par on some collections, careful on granularity
     * scala test, add tests
     * refactor into a single for and get rid of flatten?
@@ -20,11 +24,14 @@ object GenerationCore {
 
   }
 
+  val parallelExecutions = new AtomicInteger()
+  val maxParallelExecutions = Runtime.getRuntime.availableProcessors()
+
   private def _solutions(input: Input)(picksSoFar: Set[Position])(minPositionByPiece: Map[Piece, Position]): Future[Seq[PotentialSolution]] = {
     val Input(table, pieces: Seq[Piece], positions: Positions) = input
+    implicit val ec = ExecutionContext.global
 
-    def __solutions(piece: Piece, minPositionForPiece: Position, remainingPieces: Seq[Piece]) = {
-      implicit val ec = ExecutionContext.global
+    def __solutions(piece: Piece, minPositionForPiece: Position, remainingPieces: Seq[Piece]): Future[Seq[PotentialSolution]] = {
       val r: Seq[Future[Seq[PotentialSolution]]] =
         for (position: PositionInt <- positions.filter(pos => pos >= minPositionForPiece).toSeq; //todo try BitSet.range intersect with positions?
              incompatiblePositions = piece.attackPositions(position, table);
@@ -53,8 +60,15 @@ object GenerationCore {
       val piece: Piece = pieces.head
       val minPositionForPiece = minPositionByPiece(piece)
       val remainingPieces: Seq[Piece] = pieces.tail
-      implicit val ec = ExecutionContext.global
-      Future(__solutions(piece, minPositionForPiece, remainingPieces)).flatten
+      val eventualSolutionsSupplier: () => Future[Seq[PotentialSolution]] = () => __solutions(piece, minPositionForPiece, remainingPieces)
+      if (remainingPieces.size > 1 && positions.size > 9 && parallelExecutions.incrementAndGet() <= maxParallelExecutions) {
+        val future = Future(eventualSolutionsSupplier.apply()).flatten
+        future.onComplete(_ => parallelExecutions.decrementAndGet())
+        future
+      } else {
+        eventualSolutionsSupplier.apply()
+      }
+
     }
   }
 }
