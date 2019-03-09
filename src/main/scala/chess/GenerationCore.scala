157f5c3f8843de38dbd1b15befbbe066e257b600
nicu <nicolae.marasoiu@gmail.com>
2019-03-09 06:13:47 +0200

    iterable, memory taking care + optimize

--- src/main/scala/chess/GenerationCore.scala
+++ src/main/scala/chess/GenerationCore.scala
@@ -13,8 +13,8 @@ object GenerationCore {
     * todo:
     * taking a minimum, ideally constant amount of memory per level, so that the program can run in decent Xmx e.g. use Iterables not Seq, BitSet not Set etc
     * gc too much - sometimes a sinusoidal, while for other ones flat between 9% - 21%
-        * check parallelism / granularity of tasks: remainingPieces>2 may be too small for some configs & thrashing is too much, gc spiking
-        * i have about 20% GC, to investigate the causes of thrashing
+    * check parallelism / granularity of tasks: remainingPieces>2 may be too small for some configs & thrashing is too much, gc spiking
+    * i have about 20% GC, to investigate the causes of thrashing
     * add edge-case tests with zero and negative numbers/dimensions etc
     * i got 2 different results for the number of the solutions for the 7x7 problem, both a bit over 10M but still different...
     * document the trade-offs between performance / memory / type power (e.g. avoided a Position(Int,Int) case class which also took care of Position Int <-> (Int,Int) conversion via companion object, in favor of  two functions to convert directly without allocating memory..maybe it should be put back for a better model?)
@@ -23,7 +23,8 @@ object GenerationCore {
     * done?: use async testing? or apply this in tests?: https://monix.io/docs/2x/best-practices/blocking.html#if-blocking-use-scalas-blockcontext (currently blocking in tests seems in an ok way)
     */
   def solutions(input: Input): Observable[PotentialSolution] = {
-    val observable = _solutions(input)(Set())(Map[Piece, Position]().withDefaultValue(Position.zero))
+    val positionZero = Position.zero(input.table)
+    val observable = _solutions(input)(Set())(Map[Piece, Position]().withDefaultValue(positionZero))
     if (devMode) {
       import monixImplicits.global
       observable.foreach(println)
@@ -36,13 +37,13 @@ object GenerationCore {
 
     def __solutions(piece: Piece, minPositionForPiece: Position, remainingPieces: OrderedPiecesWithCount): Observable[PotentialSolution] = {
       Observable.fromIterable(
-        for (positionInt: Int <- positions.toIterable if minPositionForPiece.xy <= positionInt;
-             position = Position(positionInt)
+        for (positionInt: Int <- positions.from(minPositionForPiece.xy).toIterable;
+             position = Position(positionInt,table)
              if !picksSoFar.exists { case PiecePosition(_, otherPosition) => piece.takes(position, otherPosition) };
              incompatiblePositions = piece.incompatiblePositions(position, table);
              remainingPositions = positions &~ incompatiblePositions;
              remainingInput = Input(table, remainingPieces, remainingPositions);
-             remainingMinPosByPiece = minPositionByPiece.updated(piece, Position(position.xy + 1));
+             remainingMinPosByPiece = minPositionByPiece.updated(piece, Position(position.xy + 1,table));
              newPicks = picksSoFar + PiecePosition(piece, position))
           yield _solutions(remainingInput)(newPicks)(remainingMinPosByPiece)).flatten
     }
