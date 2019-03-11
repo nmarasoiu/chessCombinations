fe8a2f87505a0da912a2ab0b3e53c7d03e4b434f
nicolae_marasoiu <nicolae.marasoiu@gmail.com>
2019-03-11 16:04:26 +0000

    remove map.size, put parallelism at origin (bigger tasks to be subscribed as independent computation task)

--- src/main/scala/chess/GenerationCore.scala
+++ src/main/scala/chess/GenerationCore.scala
@@ -18,44 +18,40 @@ object GenerationCore {
   }
 
   private def _solutions(input: Input, picksSoFar: List[PiecePosition], minPositionByPiece: Map[Piece, Position]): Flowable[PotentialSolution] = {
-    val Input(table, pieces: OrderedPiecesWithCount, positions: Positions) = input
-    if (pieces.isEmpty) {
-      just(PotentialSolution(picksSoFar))
-    } else {
-      __solutions(table, picksSoFar, positions, pieces, minPositionByPiece)
+    Flowable.fromIterable(asJava(underlyingSolutions(input, picksSoFar, minPositionByPiece)))
+      .flatMap(a => a)
+  }
+
+  def minOptional[K, V](map: Map[K, V])(implicit cmp: Ordering[K]): Option[(K, V)] = {
+    val none: Option[(K, V)] = None
+    map.foldLeft(none) {
+      case (None, kv) => Some(kv)
+      case (Some((k1, v1)), (k2, v2)) => if (cmp.compare(k1, k2) <= 0) Some(k1, v1) else Some(k2, v2)
     }
   }
 
-  private def __solutions(table: Table,
-                          picksSoFar: List[PiecePosition],
-                          positions: Positions,
-                          pieces: OrderedPiecesWithCount,
-                          minPositionByPiece: Map[Piece, Position]): Flowable[PotentialSolution] = {
-    Flowable.just(())
-      .flatMapIterable(_ => asJava(underlyingSolutions(table, picksSoFar, positions, pieces, minPositionByPiece)))
-      .flatMap {
-        case (taskSize: Long, childFlowable: Flowable[PotentialSolution]) =>
-          if (taskSize >= 90) childFlowable.subscribeOn(Schedulers.computation()) else childFlowable
-      }
-  }
-
-  private def underlyingSolutions(table: Table,
+  private def underlyingSolutions(input:Input,
                                   picksSoFar: List[PiecePosition],
-                                  positions: Positions,
-                                  pieces: OrderedPiecesWithCount,
-                                  minPositionByPiece: Map[Piece, Position]): Iterable[(Long, Flowable[PotentialSolution])] = {
-    val (piece, pieceCount) = pieces.min
-    val remainingPieces = if (pieceCount == 1) pieces - piece else pieces + (piece -> (pieceCount - 1))
-    for (positionInt: Position <- toIterable(positions.from(minPositionByPiece(piece)));
-         positionPair = Position.fromIntToPair(positionInt, table)
-         if !picksSoFar.exists { case PiecePosition(_, otherPosition) => piece.takes(positionPair, otherPosition) })
-      yield {
-        val remainingPositions = positions &~ piece.incompatiblePositions(positionPair._1, positionPair._2, table)
-        val remainingInput = Input(table, remainingPieces, remainingPositions)
-        val remainingMinPosByPiece: Map[Piece, Position] = minPositionByPiece.updated(piece, positionInt + 1)
-        val newPicks = PiecePosition(piece, positionPair) :: picksSoFar
-        (remainingPositions.size.toLong * remainingPieces.size, _solutions(remainingInput, newPicks, remainingMinPosByPiece))
-      }
+                                  minPositionByPiece: Map[Piece, Position]): Iterable[Flowable[PotentialSolution]] = {
+    val Input(table, pieces: OrderedPiecesWithCount, positions: Positions) = input
+    minOptional(pieces) match {
+      case None =>
+        Iterable(just(PotentialSolution(picksSoFar)))
+      case Some((piece, pieceCount)) =>
+        val remainingPieces = if (pieceCount == 1) pieces - piece else pieces + (piece -> (pieceCount - 1))
+        for (positionInt: Position <- toIterable(positions.from(minPositionByPiece(piece)));
+             positionPair = Position.fromIntToPair(positionInt, table)
+             if !picksSoFar.exists { case PiecePosition(_, otherPosition) => piece.takes(positionPair, otherPosition) })
+          yield {
+            val remainingPositions = positions &~ piece.incompatiblePositions(positionPair._1, positionPair._2, table)
+            val remainingInput = Input(table, remainingPieces, remainingPositions)
+            val remainingMinPosByPiece: Map[Piece, Position] = minPositionByPiece.updated(piece, positionInt + 1)
+            val newPicks = PiecePosition(piece, positionPair) :: picksSoFar
+            val taskSize = remainingPositions.size.toLong * remainingPieces.size
+            val subSolutions = _solutions(remainingInput, newPicks, remainingMinPosByPiece)
+            if (taskSize > 99) subSolutions.subscribeOn(Schedulers.computation()) else subSolutions
+          }
+    }
   }
 
   private def asJava[T](scalaIterable: Iterable[T]): lang.Iterable[T] = {
