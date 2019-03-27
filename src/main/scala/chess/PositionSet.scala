1781cbb8a84853a7475dd02965b10d20b8b886a4
nicu <nicolae.marasoiu@gmail.com>
2019-03-27 09:55:09 +0200

    new iteratorFrom implementation, inspired from bitSet.foreach/forall, faster between 5% and 1000% compared to bitSet.iteratorFrom (when most bits are 0, this implementation is 10x faster)

--- src/main/scala/chess/PositionSet.scala
+++ src/main/scala/chess/PositionSet.scala
@@ -1,12 +1,11 @@
 package chess
 
-import java.{lang, util}
-
 import chess.extensions.CollectionsExtensions.IteratorFactoryBasedIterable
 import chess.model.Position
 
-import scala.collection.JavaConverters._
+import scala.annotation.tailrec
 import scala.collection.immutable.BitSet
+import scala.collection.{AbstractIterator, Iterator}
 
 case class PositionSet(bitSet: BitSet) {
 
@@ -16,15 +15,73 @@ case class PositionSet(bitSet: BitSet) {
 
   def intersects(that: PositionSet): Boolean = (bitSet & that.bitSet).nonEmpty
 
-
   def iterableFrom(minPosition: Position): Iterable[Position] = {
-    IteratorFactoryBasedIterable(
-      () => bitSet.iteratorFrom(minPosition.value))
-      .map(Position(_))
+    IteratorFactoryBasedIterable(() => iteratorFrom(minPosition))
   }
+
+  def iteratorFrom(minPosition: Position): Iterator[Position] = {
+    import chess.PositionSet.RichBitSet
+    bitSet.iteratorFromImproved(minPosition.value).map(Position(_))
+  }
+
 }
 
 object PositionSet {
+
+  implicit final class RichBitSet(bitSet: BitSet) {
+    val words: Array[Long] = bitSet.toBitMask
+    val len: Int = words.length
+
+    def iteratorFromImproved(start: Int): Iterator[Int] = keysIteratorFromImproved(start)
+
+    def keysIteratorFromImproved(start: Int): Iterator[Int] = new AbstractIterator[Int] {
+      private var possibleElem: Int = start
+      private final val WordLen = 64
+      private var iWord: Int = start / WordLen
+      private var wordInitialized: Boolean = false
+      private var word: Long = 0
+
+      @tailrec
+      override final def hasNext: Boolean = {
+        if (iWord >= len) {
+          false
+        } else {
+          if (!wordInitialized) {
+            word = words(iWord) >>> (start % WordLen)
+            wordInitialized = true
+          }
+          if (word == 0L) {
+            iWord += 1
+            if (iWord < len) {
+              word = words(iWord)
+              possibleElem = iWord * WordLen
+            }
+            hasNext
+          } else if ((word & 1L) == 1L) {
+            true
+          } else {
+            moveCursor()
+            hasNext
+          }
+        }
+      }
+
+      override def next(): Int = {
+        val elem = possibleElem
+        moveCursor()
+        elem
+      }
+
+      def moveCursor(): Unit = {
+        assert(word != 0L)
+        word = word >>> 1
+        possibleElem += 1
+      }
+    }
+
+
+  }
+
   val empty = PositionSet(BitSet())
 
   def apply(): PositionSet = empty
--- src/test/scala/chess/PositionInTableProperties.scala
+++ src/test/scala/chess/PositionInTableProperties.scala
@@ -3,8 +3,37 @@ package chess
 import chess.model._
 import org.scalatest.FunSuite
 
+import scala.collection.immutable.BitSet
+import scala.util.Random
+
 class PositionInTableProperties extends FunSuite {
   def inOrder(a: Int, b: Int, c: Int): Boolean = a <= b && b <= c
+
+  test("BitSet fromIterator") {
+    for (len <- Seq(7, 3, 2, 1, 0, 4, 8, 9, 43, 12, 14, 2100, 3200);
+         elems = Stream.continually(Random.nextInt(9000)).take(len);
+         startIndex <- if (len == 0) Seq(0) else Stream.continually(Random.nextInt(len)).take(12);
+         bitSet = BitSet(elems: _*);
+         _ <- 1 to 12) {
+      import chess.PositionSet.RichBitSet
+      val myImplRes = doRepeatedly(bitSet.keysIteratorFromImproved(startIndex).toVector)
+      val scalaImplRes = doRepeatedly(bitSet.keysIteratorFrom(startIndex).toVector)
+      println("-----")
+      assert(myImplRes == scalaImplRes)
+    }
+  }
+
+  def doRepeatedly[E](expr: => E): E = {
+    var i = 0
+    val t0 = System.currentTimeMillis()
+    while (i < 1) {//set this to like 9999 to see differences; my implementation (inspired from foreach/forall, gets 10 times faster on few elements / many zeroes)
+      expr
+      i += 1
+    }
+    val t1 = System.currentTimeMillis()
+    println("Time to execute: " + (t1 - t0))
+    expr
+  }
 
   test("Table should serialize to int and deserialize back the same") {
     for (positionInt: Int <- Seq(0, 3, 4, 5, 12, 127, 128, 130);
